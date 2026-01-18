04276f9f4c3caf2d1419967234d59d2f3495fd58
Richard Hamilton Carrion Rodriguez <rcarrion@jbgye.org.ec>
2026-01-17 19:27:38 -0500

    Chore: Add legacy support for Android 9 and Chrome 83

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,18 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 import tailwindcss from '@tailwindcss/postcss'
 import autoprefixer from 'autoprefixer'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/ruleta/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['chrome >= 83', 'android >= 9'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -21,4 +28,7 @@ export default defineConfig({
       ],
     },
   },
+  build: {
+    target: ['es2020', 'edge88', 'firefox78', 'chrome87', 'safari14'],
+  }
 })
