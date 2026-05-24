888bfd5cfd15cc97c03626db63a123fc5ee08469
Richard Carrion <richard.carrion.r@gmail.com>
2026-05-23 19:29:57 -0500

    Crear archivo para almacenar información del commit

---
+++ src/utils/commit-info.ts
@@ -0,0 +1,5 @@
+// Este archivo es generado automáticamente durante el build
+// Se inyecta el hash del commit actual
+
+export const COMMIT_HASH = import.meta.env.VITE_COMMIT_HASH || 'development';
+export const COMMIT_URL = `https://github.com/rcarrionr/ruleta/commit/${COMMIT_HASH}`;
